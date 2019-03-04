10740e0ec71ca7c534d8d26f05759a7a7f57d6d2
Max <savagecm@users.noreply.github.com>
2019-03-04 16:13:07 +0800

    aa

---
+++ code/decl_type.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+template <typename T, typename U>
+auto add(T t, U u) -> decltype(t + u) {
+  return t + u;
+}
+
+int main() {
+  std::string s1("Hello, "), s2("World");
+  auto x = add(1, 2.3);
+  auto y = add(s1, s2);
+  //  auto z = add(s1, 1.0); // Fails - std::string + double is not defined
+
+  std::cout << x << std::endl;
+  std::cout << y << std::endl;
+
+  return 0;
+}
