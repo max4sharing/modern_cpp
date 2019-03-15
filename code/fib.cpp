edb4f62a36671f4b21c9dc71a39967e9c3992261
Max <savagecm@users.noreply.github.com>
2019-03-15 16:08:24 +0800

    aa

--- code/fib.cpp
+++ code/fib.cpp
@@ -1,20 +1,7 @@
-#include <iostream>
-#include <tuple>
+template<bool T>
+class MagicType {
+    bool magic = T;
+};
 
-std::tuple<int, double, std::string> f()
-{
-    return std::make_tuple(1, 2.3, "456");
-}
-
-int main()
-{
-    // C++17
-    auto [x, y, z] = f();
-    std::cout << x << ", " << y << ", " << z << std::endl;
-    //C++11
-    int a, b, c;
-    std::tie(a, b, c) = f();
-
-    std::cout << a << ", " << b << ", " << c << std::endl;
-    return 0;
-}
+// in main function:
+std::vector<MagicType<(1>2)>> magic;
